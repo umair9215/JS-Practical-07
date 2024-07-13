9fc396a0a739380bae067106d4a26f6ef68573e1
Malik Muhammad Umair <167450073+umair9215@users.noreply.github.com>
2024-07-12 17:40:09 -0700

    JavaScript Practical 07 Done by Malik Muhammad Umair

---
+++ script.js
@@ -0,0 +1,28 @@
+// let a = prompt("Enter your message!");
+// let b = prompt("Enter the character to extract");
+// let c = a.charAt(b);
+// alert(c);
+
+
+
+// let a = prompt("Enter a massage");
+// let b = prompt("Enter text to replace");
+// let c = prompt("Enter new text");
+// let d = a.replace(b, c);
+// alert(d);
+
+
+
+// let num = prompt ("Enter a decimal number");
+// num = Math.round(num);
+// alert(num);
+
+
+// let num = prompt ("Enter a decimal number");
+// num = Math.ceil(num);
+// alert(num);
+
+
+let num = prompt ("Enter a decimal number");
+num = Math.floor(num);
+alert(num);
